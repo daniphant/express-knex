2b289f3eea606ce127586b9d1febfbd98d6964e8
daniphant <daniphant@protonmail.com>
2021-10-28 20:30:49 -0300

    feat: add get user endpoints

---
+++ src/router.ts
@@ -0,0 +1,11 @@
+import Router from "express";
+import { createUserController } from "./useCases/CreateUser";
+import { getUserController } from "./useCases/GetUser";
+
+const router = Router();
+
+// Get all users
+router.get("/users", (req, res) => getUserController.handle(req, res));
+
+// Get user by id;
+router.get("/users/:id", (req, res) => getUserController.handleOne(req, res));
