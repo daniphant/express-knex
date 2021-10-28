509992e6edf90b3891c6bedd34e7fa8b86c9f0fa
daniphant <daniphant@protonmail.com>
2021-10-28 20:36:49 -0300

    feat: add create user endpoint

--- src/router.ts
+++ src/router.ts
@@ -10,4 +10,7 @@ router.get("/users", (req, res) => getUserController.handle(req, res));
 // Get user by id;
 router.get("/users/:id", (req, res) => getUserController.handleOne(req, res));
 
+// Create a new user
+router.post("/users", (req, res) => createUserController.handle(req, res));
+
 export { router };
