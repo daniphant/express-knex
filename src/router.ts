142be71dc5d80a751eac8d8d6cd9cfc0e5422672
daniphant <daniphant@protonmail.com>
2021-10-28 21:16:21 -0300

    feat: add update and delete user endpoints

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import Router from "express";
 import { createUserController } from "./useCases/Users/CreateUser";
 import { deleteUserController } from "./useCases/Users/DeleteUser";
 import { getUserController } from "./useCases/Users/GetUser";
+import { updateUserController } from "./useCases/Users/UpdateUser";
 
 const router = Router();
 
@@ -13,5 +14,13 @@ router.get("/users/:id", (req, res) => getUserController.handleOne(req, res));
 
 // Create a new user
 router.post("/users", (req, res) => createUserController.handle(req, res));
+
+// Update user by id
+router.put("/users/:id", (req, res) => updateUserController.handle(req, res));
+
+// Delete user by id
+router.delete("/users/:id", (req, res) =>
+  deleteUserController.handle(req, res),
+);
 
 export { router };
